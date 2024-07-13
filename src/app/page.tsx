c91221e56d33bbd15ccb2b286e038dd1d38c1872
utkarsh <r.utkarsh.0010@gmail.com>
2024-07-13 14:33:32 +0530

    add CTA section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { CTASection } from "@/components/CTASection";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <div className="poppins">
       <Navbar />
+      <CTASection />
     </div>
   );
 }
---
+++ src/components/CTASection/index.tsx
@@ -0,0 +1,22 @@
+import {
+  CTA_TITLE_1,
+  CTA_TITLE_2,
+  CTA_DESCRIPTION,
+  CTA_BUTTON,
+} from "@/constants/ctaSection";
+
+export function CTASection() {
+  return (
+    <div className="flex flex-col mx-16 items-center mt-[120px] gap-6">
+      <h2 className="font-bold text-2xl text-center">
+        {CTA_TITLE_1}
+        <span className="text-theme px-1">{CTA_TITLE_2}</span>
+      </h2>
+      <p className="text-center text-sm w-[450px] max-sm:w-full text-slate-500">
+        {CTA_DESCRIPTION}
+      </p>
+
+      <button className="block px-9 py-3 text-sm font-medium text-white transition duration-400 focus:outline-none" type="button">{CTA_BUTTON}</button>
+    </div>
+  );
+}
