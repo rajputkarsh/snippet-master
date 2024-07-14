3049bfa9645aa535eab543a5d1130b8b90a8ef7c
utkarsh <r.utkarsh.0010@gmail.com>
2024-07-15 01:04:36 +0530

    add dark mode component

---
+++ src/components/Topbar/DarkMode.tsx
@@ -0,0 +1,39 @@
+import { useGlobalContext } from "@/context";
+
+function DarkMode() {
+  const {
+    darkModeObject: { darkMode, setDarkMode },
+  } = useGlobalContext();
+
+  const handleClick = (id: number) => {
+    const updatedModes = darkMode.map((item) => {
+      if (item.id === id) {
+        return { ...item, isSelected: true };
+      } else {
+        return { ...item, isSelected: false };
+      }
+    });
+
+    setDarkMode(updatedModes);
+  };
+
+  return (
+    <div className="bg-slate-100 h-[36px] w-[74px] rounded-3xl flex items-center gap-2 pl-[5px]">
+      {darkMode.map((item) => (
+        <div
+          className={`${
+            item.isSelected
+              ? "bg-purple-600 text-white"
+              : "bg-slate-100 text-purple-600"
+          } w-7 h-7 flex items-center justify-center rounded-full top-[4px] p-1 left-1 cursor-pointer select-none`}
+          key={`darkmode_${item.id}`}
+          onClick={() => handleClick(item.id)}
+        >
+          {item.icon}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default DarkMode;
