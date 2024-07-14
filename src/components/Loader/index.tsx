31c297d7bd1ce4fdd863ad5bd1ba7a3037927ae1
utkarsh <r.utkarsh.0010@gmail.com>
2024-07-14 18:42:07 +0530

    add lottie loader

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import Lottie from "lottie-web";
+import loader from "../../assets/loader.json";
+
+interface LoaderProps {
+  timer?: number;
+}
+
+function Loader({ timer = 30 }: LoaderProps) {
+  const [showLoader, setShowLoader] = useState<boolean>(true);
+
+  useEffect(() => {
+    const lottieInstance = Lottie.loadAnimation({
+      container: document.querySelector("#lottie-loader") as Element,
+      animationData: loader,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+    });
+
+    setTimeout(() => {
+      setShowLoader(false);
+    }, timer * 1000);
+
+    return () => lottieInstance.destroy();
+  }, []);
+
+  return (
+    <>
+      {showLoader && (
+        <div className="loader">
+          <div id="lottie-loader"></div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default Loader;
