045cdf3520151dd2188e36229e4d2679c6317011
utkarsh <r.utkarsh.0010@gmail.com>
2024-07-17 23:57:34 +0530

    update sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -7,13 +7,16 @@ import { isDarkMode } from "@/lib/utils";
 function SideBar() {
   const {
     darkModeObject: { darkMode },
+    openSidebarObject: {openSidebar, setOpenSidebar},
   } = useGlobalContext();
 
   const isDarkModeEnabled = isDarkMode(darkMode);
 
   return (
     <div
-      className={`max-md:hidden pr-10 p-6 flex flex-col gap-2 h-screen pt-7 ${
+      className={`${
+        openSidebar ? "fixed z-50 shadow-lg" : "max-md:hidden"
+      } pr-10 p-6 flex flex-col gap-2 h-screen pt-7 ${
         isDarkModeEnabled ? "bg-slate-800" : "bg-white"
       }`}
     >
--- src/components/Topbar/SidebarMenuIcon.tsx
+++ src/components/Topbar/SidebarMenuIcon.tsx
@@ -1,7 +1,23 @@
-import { MenuOutlined } from "@mui/icons-material";
+import { useGlobalContext } from "@/context";
+import { MenuOutlined, CloseOutlined } from "@mui/icons-material";
 
 export default function SidebarMenuIcon() {
+  const {
+    openSidebarObject: { openSidebar, setOpenSidebar },
+  } = useGlobalContext();
   return (
-    <MenuOutlined className="text-slate-500 cursor-pointer max-md:block hidden" />
+    <>
+      {!openSidebar ? (
+        <MenuOutlined
+          onClick={() => setOpenSidebar((_) => !openSidebar)}
+          className="text-slate-500 cursor-pointer max-md:block hidden"
+        />
+      ) : (
+        <CloseOutlined
+          onClick={() => setOpenSidebar((_) => !openSidebar)}
+          className="text-slate-500 cursor-pointer max-md:block hidden"
+        />
+      )}
+    </>
   );
 }
