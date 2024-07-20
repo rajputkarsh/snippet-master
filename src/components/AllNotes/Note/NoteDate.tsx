235cbb6ba3f69a6d7aa2f539096549067ef890c3
utkarsh <r.utkarsh.0010@gmail.com>
2024-07-20 20:41:17 +0530

    add NoteDate component

--- src/components/AllNotes/Note/NoteDate.tsx
+++ src/components/AllNotes/Note/NoteDate.tsx
@@ -1,8 +1,12 @@
 
 function NoteDate() {
   return (
-    <div>NoteDate</div>
-  )
+    <div className="text-slate-500 text-[11px] flex gap-1 font-light mx-4 mt-1">
+      <span className="">23th June 2024</span>
+      <span className="">*</span>
+      <span className="">24 mins ago</span>
+    </div>
+  );
 }
 
 export default NoteDate
