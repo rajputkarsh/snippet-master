d69d1679ac95578976af80b8e6515b4d52797920
utkarsh <r.utkarsh.0010@gmail.com>
2024-07-27 05:46:02 +0530

    add + Snippet button functionality

--- src/components/Topbar/AddSnippetButton.tsx
+++ src/components/Topbar/AddSnippetButton.tsx
@@ -1,17 +1,37 @@
 
 import { useGlobalContext } from "@/context";
 import { SNIPPET } from "@/constants/config";
+import { SingleNoteType } from "@/interfaces/context";
 
 export default function AddSnippetButton() {
   const {
-    darkModeObject: { darkMode },
-    openNoteContentObject: { openNoteContent },
-    isMobileObject: { isMobile },
-    allNotesObject: { allNotes },
+    allNotesObject: { allNotes, setAllNotes },
+    selectedNoteObject: { selectedNote, setSelectedNote },
+    openNoteContentObject: { openNoteContent, setOpenNoteContent },
   } = useGlobalContext();  
 
+  const openNewNoteContent = () => {
+    const newSingleNote: SingleNoteType = {
+      id: crypto.randomUUID(),
+      title: "",
+      isFavorite: false,
+      tags: [],
+      description: "",
+      code: "",
+      language: "",
+      createdOn: "",      
+    };
+
+    setAllNotes((_) => [...allNotes, newSingleNote]);
+    setSelectedNote((_) => newSingleNote);
+    setOpenNoteContent((_) => true);
+  }
+
   return (
-    <div className="absolute flex gap-2 px-3 rounded-3xl bg-purple-600 p-1 text-[13px] text-white top-[5px] right-[6px] items-center cursor-pointer select-none">
+    <div
+      onClick={openNewNoteContent}
+      className="absolute flex gap-2 px-3 rounded-3xl bg-purple-600 p-1 text-[13px] text-white top-[5px] right-[6px] items-center cursor-pointer select-none"
+    >
       <div className="font-bold">+</div>
       <div className="max-md:hidden">{SNIPPET}</div>
     </div>
