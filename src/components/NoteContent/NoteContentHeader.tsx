c239e723efd1e16502600e665325aa6ff6b9e11d
utkarsh <r.utkarsh.0010@gmail.com>
2024-08-04 21:13:12 +0530

    update note content header title

--- src/components/NoteContent/NoteContentHeader.tsx
+++ src/components/NoteContent/NoteContentHeader.tsx
@@ -7,6 +7,8 @@ import {
 } from "react";
 import { useGlobalContext } from "@/context";
 import { SingleNoteType } from "@/interfaces/context";
+import { TitleOutlined } from "@mui/icons-material";
+import { TEXT_AREA_PLACEHOLDER } from "@/constants/note";
 
 interface NoteContentHeaderProps {
   singleNote: SingleNoteType;
@@ -25,18 +27,18 @@ function NoteContentHeader({
 
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
   
-  const handleUpdate = (event: ChangeEvent<HTMLInputElement>): void => {
+  const handleUpdate = (event: ChangeEvent<HTMLTextAreaElement>) => {
     const newSingleNote = { ...singleNote, title: event.target.value };
     setSingleNote(newSingleNote);
 
     const newAllNotes = allNotes.map((note) => {
-      if(note.id === newSingleNote.id) {
+      if (note.id === newSingleNote.id) {
         return newSingleNote;
       }
       return note;
     });
     setAllNotes((_) => newAllNotes);
-  }
+  };
 
   const handleKeyDown = (event: KeyboardEvent<HTMLTextAreaElement>) => {
     if (event.key === 'Event') {
@@ -45,12 +47,31 @@ function NoteContentHeader({
   }
 
   return (
-    <input
-      placeholder="Add new title"
-      value={singleNote.title}
-      onChange={handleUpdate}
-    />
+    <div className="flex justify-between gap-8 mb-4">
+      <div className="flex gap-2 w-full">
+        <TitleOutlined
+          sx={{ fontSize: 19 }}
+          className="text-slate-400 mt-[4px]"
+        />
+        <textarea
+          ref={textAreaRef}
+          placeholder={TEXT_AREA_PLACEHOLDER}
+          value={singleNote.title}
+          onChange={handleUpdate}
+          onKeyDown={handleKeyDown}
+          className="font-bold text-xl outline-none resize-none h-auto overflow-hidden w-full"
+        />
+      </div>
+    </div>
   );
+
+  // return (
+  //   <input
+  //     placeholder="Add new title"
+  //     value={singleNote.title}
+  //     onChange={handleUpdate}
+  //   />
+  // );
 }
 
 export default NoteContentHeader;
