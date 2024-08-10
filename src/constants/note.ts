a67193103144bd580085c101e47dd5a41514ffb7
utkarsh <r.utkarsh.0010@gmail.com>
2024-08-10 23:11:02 +0530

    add no TagsMenu

--- src/constants/note.ts
+++ src/constants/note.ts
@@ -1,3 +1,10 @@
 
 export const TEXT_AREA_PLACEHOLDER = "New Title...";
 export const NO_TAGS_TEXT = "No Tags";
+
+export const AVAILABLE_TAGS: Array<{ id: string; name: string }> = [
+  { id: "1", name: "Tag 1" },
+  { id: "2", name: "Tag 2" },
+  { id: "3", name: "Tag 3" },
+  { id: "4", name: "Tag 4" },
+];
