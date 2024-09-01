c8efe397a24a4e8b8a45870f434bd4d86106b7f4
utkarsh <r.utkarsh.0010@gmail.com>
2024-09-01 10:56:01 +0530

    fix AddTag UI in dark mode

--- src/components/AddTag/Header.tsx
+++ src/components/AddTag/Header.tsx
@@ -1,11 +1,15 @@
 import { ADD_NEW_TAG_HEADER } from "@/constants/tags";
 import { useGlobalContext } from "@/context"
+import { isDarkMode } from "@/lib/utils";
 import { Close } from "@mui/icons-material";
 
 function Header() {
   const {
     openNewTagsWindowObject: { setOpenNewTagsWindow },
+    darkModeObject: { darkMode },
   } = useGlobalContext();
+
+  const isDarkModeEnabled = isDarkMode(darkMode);
 
   const closeDialog = () => {
     setOpenNewTagsWindow(() => false);
@@ -14,19 +18,19 @@ function Header() {
   return (
     <div className="flex justify-between items-center rounded-lg">
       <div className="flex items-center gap-2">
-        <span className="text-[18px] text-slate-600 font-bold">
+        <span className={`text-[18px] font-bold ${isDarkModeEnabled ? "text-white" : "text-slate-600"}`}>
           {ADD_NEW_TAG_HEADER}
         </span>
       </div>
       <div>
-        <Close 
-          sx={{fontSize: 18}}
+        <Close
+          sx={{ fontSize: 18 }}
           className="text-slate-400 cursor-pointer"
           onClick={closeDialog}
         />
       </div>
     </div>
-  )
+  );
 }
 
 export default Header
