c5ccf06633324caf6fd0c8933cfd719672f3a1f6
utkarsh <r.utkarsh.0010@gmail.com>
2024-09-01 11:06:37 +0530

    add AddTag input component

--- src/components/AddTag/TagInput.tsx
+++ src/components/AddTag/TagInput.tsx
@@ -1,7 +1,43 @@
+import { useEffect, useRef, useState } from "react";
+import { useGlobalContext } from "@/context";
+import {
+  TAG_NAME_LABEL_TEXT,
+  TAG_NAME_PLACEHOLDER_TEXT,
+} from "@/constants/tags";
+import { isDarkMode } from "@/lib/utils";
+
 function TagInput() {
+  const {
+    darkModeObject: { darkMode },
+  } = useGlobalContext();
+
+  const isDarkModeEnabled = isDarkMode(darkMode);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [tagName, setTagName] = useState<string>("");
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
+
   return (
-    <div>TagInput</div>
-  )
+    <div className="mt-6">
+      <span className="text-slate-400 text-sm font-semibold">
+        {TAG_NAME_LABEL_TEXT}
+      </span>
+      <input
+        ref={inputRef}
+        value={tagName}
+        onChange={(e) => setTagName(e.target.value)}
+        placeholder={TAG_NAME_PLACEHOLDER_TEXT}
+        className={`${
+          isDarkModeEnabled ? "bg-slate-700" : "bg-white border text-slate-600"
+        } w-full rounded-md p-2 mt-1 text-[12px] outline-none`}
+      />
+    </div>
+  );
 }
 
-export default TagInput
+export default TagInput;
--- src/constants/tags.ts
+++ src/constants/tags.ts
@@ -1,3 +1,5 @@
 export const TAGS_SEARCH_INPUT_PLACEHOLDER = "Search a tag...";
 export const TAGS_SEARCH_BUTTON_TEXT = "Add Tags";
 export const ADD_NEW_TAG_HEADER = "Add new Tag";
+export const TAG_NAME_LABEL_TEXT = "Tag Name";
+export const TAG_NAME_PLACEHOLDER_TEXT = "For Example, Login Form";
