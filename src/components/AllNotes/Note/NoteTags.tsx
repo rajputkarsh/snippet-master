f738ba7ff09d0439914156cd528a12978243790d
utkarsh <r.utkarsh.0010@gmail.com>
2024-09-08 12:39:00 +0530

    update note tags UI

--- src/components/AllNotes/Note/NoteTags.tsx
+++ src/components/AllNotes/Note/NoteTags.tsx
@@ -1,4 +1,6 @@
+import { useGlobalContext } from "@/context";
 import { SingleTagType } from "@/interfaces/context";
+import { isDarkMode } from "@/lib/utils";
 
 interface NoteTagsProps {
   id: string;
@@ -6,12 +8,22 @@ interface NoteTagsProps {
 }
 
 function NoteTags({ id, tags }: NoteTagsProps) {
+  const {
+    darkModeObject: { darkMode },
+  } = useGlobalContext();
+
+  const isDarkModeEnabled = isDarkMode(darkMode);
+
   return (
-    <div className="text-slate-500 text-[11px] mx-4 flex flex-wrap gap-1 mt-4">
+    <div className="text-[11px] mx-4 flex flex-wrap gap-1 mt-4">
       {tags.map((tag) => (
         <span
           key={`${id}_${tag.id}`}
-          className="bg-purple-100 text-theme p-1 rounded-md px-2"
+          className={` p-1 rounded-md px-2 font-semibold ${
+            isDarkModeEnabled
+              ? "bg-slate-600 text-slate-50"
+              : "bg-slate-200 text-slate-900"
+          }`}
         >
           {tag.name}
         </span>
