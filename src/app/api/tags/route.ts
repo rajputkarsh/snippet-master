61542c4834a57d3634598a32c31a50914f1f78c1
utkarsh <r.utkarsh.0010@gmail.com>
2024-09-17 00:33:43 +0530

    add tags related APIs

---
+++ src/app/api/tags/route.ts
@@ -0,0 +1,98 @@
+import { INTERNAL_SERVER_ERROR, INVALID_SNIPPET_ID, INVALID_USER_ID } from "@/constants/error";
+import { SNIPPET_DELETED_SUCCESSFULLY, SNIPPET_SAVED_SUCCESSFULLY, SNIPPET_UPDATED_SUCCESSFULLY, SNIPPETS_FETCHED_SUCCESSFULLY } from "@/constants/messages";
+import ITag from "@/interfaces/models/tag";
+import { connect } from "@/lib/database";
+import { createTag, findAllUserTag, updateTag } from "@/service/tag.service";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const tagInfo = await req.json() as ITag;
+    
+    await connect();
+    const tag = await createTag(tagInfo);
+
+    return NextResponse.json({
+      message: SNIPPET_SAVED_SUCCESSFULLY,
+      data: tag,
+    });
+
+  } catch(err: any) {
+    return NextResponse.json(
+      { error: err?.message || INTERNAL_SERVER_ERROR },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET(req: NextRequest) {
+  try {
+    const clerkUserId = req.nextUrl.searchParams.get("userId");
+
+    if (!clerkUserId) {
+      return NextResponse.json({ error: INVALID_USER_ID }, { status: 400 });
+    }
+
+    await connect();
+    const tags = await findAllUserTag(clerkUserId);
+
+    return NextResponse.json({
+      message: SNIPPETS_FETCHED_SUCCESSFULLY,
+      data: tags,
+    });
+  } catch (err: any) {
+    return NextResponse.json(
+      { error: err?.message || INTERNAL_SERVER_ERROR },
+      { status: 500 }
+    );
+  }
+}
+
+export async function PATCH(req: NextRequest) {
+  try {
+    const tagId = req.nextUrl.searchParams.get("id");
+    const tagInfo = (await req.json()) as ITag;
+
+    if (!tagId) {
+      return NextResponse.json({ error: INVALID_SNIPPET_ID }, { status: 400 });
+    }
+
+    await connect();
+    const tag = await updateTag(tagId, tagInfo);
+
+    return NextResponse.json({
+      message: SNIPPET_UPDATED_SUCCESSFULLY,
+      data: tag,
+    });
+  } catch (err: any) {
+    return NextResponse.json(
+      { error: err?.message || INTERNAL_SERVER_ERROR },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(req: NextRequest) {
+  try {
+    const tagId = req.nextUrl.searchParams.get("id");
+
+    if (!tagId) {
+      return NextResponse.json({ error: INVALID_SNIPPET_ID }, { status: 400 });
+    }
+
+    await connect();
+
+    
+    const tags = await findAllUserTag(tagId);
+
+    return NextResponse.json({
+      message: SNIPPET_DELETED_SUCCESSFULLY,
+      data: {},
+    });
+  } catch (err: any) {
+    return NextResponse.json(
+      { error: err?.message || INTERNAL_SERVER_ERROR },
+      { status: 500 }
+    );
+  }
+}
