0f4f1bbf57457747196add60022373657ce4fb0b
utkarsh <r.utkarsh.0010@gmail.com>
2024-09-18 00:09:40 +0530

    add image in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
 import { CTASection } from "@/components/CTASection";
 import Features from "@/components/Features";
 import Navbar from "@/components/Navbar";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="poppins">
       <Navbar />
       <CTASection />
-      <Features />
+      <div className="flex flex-col md:flex-row items-center justify-center gap-4">
+        <div className="w-full md:w-3/6">
+          <Features />
+        </div>
+        <div className="min-h-48 md:min-h-96 h-full w-[90%] md:w-3/6 relative">
+          <Image className="!w-auto" src="/snippet-master.png" alt="Snippet Master" fill={true} />
+        </div>
+      </div>
     </div>
   );
 }
