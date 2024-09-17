e2ee0bb2dc965af83970c692c271ed8c838d25a6
utkarsh <r.utkarsh.0010@gmail.com>
2024-09-18 00:13:02 +0530

    add footer text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,9 +13,15 @@ export default function Home() {
           <Features />
         </div>
         <div className="min-h-48 md:min-h-96 h-full w-[90%] md:w-3/6 relative">
-          <Image className="!w-auto" src="/snippet-master.png" alt="Snippet Master" fill={true} />
+          <Image
+            className="!w-auto"
+            src="/snippet-master.png"
+            alt="Snippet Master"
+            fill={true}
+          />
         </div>
       </div>
+      <div className="mt-4 md:mt-0 md:absolute bottom-2 w-full text-center font-bold text-gray-800">With ❤️, Your Fellow Dev</div>
     </div>
   );
 }
