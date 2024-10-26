5f365311711049cf1ad75efcc722fd84e5443092
utkarsh <r.utkarsh.0010@gmail.com>
2024-10-27 01:56:42 +0530

    add share model

---
+++ src/models/Share.model.ts
@@ -0,0 +1,40 @@
+import mongoose, { model, Schema } from "mongoose";
+import IShare from "../interfaces/models/share";
+
+export const shareSchema: Schema = new Schema<IShare>(
+  {
+    id: {
+      required: true,
+      type: String,
+    },
+    shareId: {
+      required: true,
+      type: String,
+    },
+    ownerId: {
+      type: String,
+      required: true,
+    },
+    viewedBy: [{
+     type: String,
+     required: false, 
+    }]
+  },
+  {
+    versionKey: false,
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  }
+);
+
+shareSchema.set("toObject", { getters: true, virtuals: true });
+shareSchema.set("toJSON", {
+  virtuals: true,
+  getters: true,
+});
+
+const ShareModel = mongoose.models.Share || model<IShare>("Share", shareSchema);
+
+export default ShareModel;
