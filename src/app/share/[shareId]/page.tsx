02d85f244ee36b04b75d0bec772a99404ce21df1
utkarsh <r.utkarsh.0010@gmail.com>
2024-10-27 04:09:23 +0530

    add shareable link logs

--- src/app/share/[shareId]/page.tsx
+++ src/app/share/[shareId]/page.tsx
@@ -1,4 +1,5 @@
 import ShareableNote from "@/components/ShareableNote";
+import { connect } from "@/lib/database";
 import { findShare } from "@/service/share.service";
 import { findSnippet } from "@/service/snippet.service";
 import { redirect } from "next/navigation";
@@ -6,6 +7,10 @@ import { Toaster } from "react-hot-toast";
 
 async function ShareableLink({ params }: { params: { shareId: string } }) {
   console.log(`inside page route`);
+
+    await connect();
+    console.log(`connected to DB`);
+
   const shareInfo = await findShare(params.shareId);
 
     console.log(`shareInfoFetched`);
