04662426691775170e08a4848bee7916623b8301
utkarsh <r.utkarsh.0010@gmail.com>
2024-10-27 02:51:44 +0530

    optimize share API

--- src/app/api/share/route.ts
+++ src/app/api/share/route.ts
@@ -1,8 +1,9 @@
 import { INTERNAL_SERVER_ERROR } from "@/constants/error";
 import { SHARE_URL_CREATED, INVALID_SNIPPET } from "@/constants/messages";
+import { SHAREABLE_LINK_VALIDITY } from "@/constants/share";
 import IShare from "@/interfaces/models/share";
 import { connect } from "@/lib/database";
-import { createShare } from "@/service/share.service";
+import { createShare, findPreviousShares } from "@/service/share.service";
 import { findSnippet } from "@/service/snippet.service";
 import { NextResponse } from "next/server";
 
@@ -23,20 +24,29 @@ export async function POST(req: Request) {
     }
 
     const validTillDate = new Date();
-    validTillDate.setMinutes(validTillDate.getMinutes() + 180);
+    validTillDate.setMinutes(validTillDate.getMinutes() + SHAREABLE_LINK_VALIDITY);
 
-    const shareInfo = await createShare({
-      id: crypto.randomUUID(),
-      shareId: snippetInfo.id,
-      ownerId: snippetInfo.clerkUserId,
-      validTill: validTillDate.toISOString(),
-      viewedBy: [],
-    });
+    let shareInfo;
+
+    const previousShareInfo = await findPreviousShares(snippetId);
+    
+    if (previousShareInfo.length) {
+      shareInfo = previousShareInfo[previousShareInfo.length - 1] as IShare;
+    } else {
+      shareInfo = await createShare({
+        id: crypto.randomUUID(),
+        snippetId: snippetInfo.id,
+        ownerId: snippetInfo.clerkUserId,
+        validTill: validTillDate.toISOString(),
+        viewedBy: [],
+      });
+    }
+
 
     return NextResponse.json({
       message: SHARE_URL_CREATED,
       data: {
-        url: `share/${shareInfo.id}`,
+        url: `/share/${shareInfo.id}`,
         validFor: "3 hours",
       },
     });
